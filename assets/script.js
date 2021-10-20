0462208045b793d33c0a139f3b3a76b19b07a36e
CharlottesHead <z.moloney80@gmail.com>
2021-10-20 16:06:55 +0000

    Fix error

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+//Check correct answers
+Question.prototype.correctAnswer = function (choice) {
+    return choice === this.answer;
+}
