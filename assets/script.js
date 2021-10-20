14edc892e2261cd29a717ca81b02389588b5c31a
CharlottesHead <z.moloney80@gmail.com>
2021-10-20 16:12:15 +0000

    Add JS functions

--- assets/script.js
+++ assets/script.js
@@ -25,3 +25,32 @@ Quiz.prototype.isEnded = function () {
     return this.questions.length === this.questionIndex;
 
 }
+
+//Check user answers
+Quiz.prototype.guess = function (answer) {
+
+    if (this.getQuestionIndex().correctAnswer(answer)) {
+        this.score++;
+    }
+    this.questionIndex++;
+
+}
+
+
+function populate() {
+    if (quiz.isEnded()) {
+        showScores();
+    } else {
+        var element = document.getElementById('question');
+        element.innerHTML = quiz.getQuestionIndex().text;
+        var choices = quiz.getQuestionIndex().choices;
+        for (var i = 0; i < choices.length; i++) {
+            var element = document.getElementById('choice' + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+
+        }
+
+        showProgress();
+    }
+}
